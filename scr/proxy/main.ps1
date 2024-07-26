b847e12e156abc3c91025ea0e91928fd1f6c42ed
43a1723 <hai1723notgay@gmail.com>
2024-07-26 15:39:06 +0700

    Update main.ps1

--- scr/proxy/main.ps1
+++ scr/proxy/main.ps1
@@ -1,24 +1,33 @@
-# URL chứa danh sách các URL proxy SOCKS4
-$proxyListUrl = "https://raw.githubusercontent.com/werearecat/proxy-gen/main/proxy4-url"
+# Định nghĩa đường dẫn tới file chứa các URL
+$filePath = "proxy-gen.dat"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/werearecat/proxy-gen/main/proxy4-url" -OutFile $filePath
+# Đọc nội dung của file vào một mảng
+$fileContent = Get-Content -Path $filePath
 
-# Lấy nội dung từ URL chứa danh sách proxy URLs
-$proxyUrls = Invoke-RestMethod -Uri $proxyListUrl
-
-# Khởi tạo một mảng để lưu nội dung từ các proxy URLs
+# Khởi tạo một danh sách để lưu nội dung từ các URL
 $allProxies = @()
 
-# Lặp qua từng URL trong danh sách
-foreach ($proxyUrl in $proxyUrls) {
-    try {
-        # Lấy nội dung từ mỗi URL proxy SOCKS4
-        $proxyContent = Invoke-RestMethod -Uri $proxyUrl
-        # Thêm nội dung vào mảng
-        $allProxies += $proxyContent
-    }
-    catch {
-        Write-Output "Không thể tải nội dung từ $proxyUrl"
+# Lặp qua từng dòng trong file
+foreach ($line in $fileContent) {
+    # Kiểm tra xem dòng có phải là URL hợp lệ không
+    if ($line -match "^https?://") {
+        try {
+            # Lấy nội dung từ mỗi URL
+            $proxyContent = Invoke-RestMethod -Uri $line
+            
+            # Thêm nội dung vào danh sách
+            $allProxies += $proxyContent
+        }
+        catch {
+            Write-Output "Không thể tải nội dung từ $line"
+        }
     }
 }
 
-# Ghi tất cả nội dung vào file proxy.txt
-$allProxies | Out-File -FilePath "proxy.txt"
+# Ghi tất cả nội dung vào file proxy4.txt
+if ($allProxies.Count -gt 0) {
+    $allProxies | Out-File -FilePath "proxy4.txt"
+    Write-Output "Nội dung đã được ghi vào file proxy4.txt"
+} else {
+    Write-Output "Không có dữ liệu để ghi vào file proxy4.txt"
+}
